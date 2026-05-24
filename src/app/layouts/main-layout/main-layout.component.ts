084bab4c326f08528f4df53e5ff331c25fe96907
SRT DEV <sahirruiz4@gmail.com>
2026-05-24 18:08:44 -0500

    feat(layout): agregar componente de diseño principal con barra lateral y encabezado

---
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,18 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../../shared/ui/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.css',
+})
+export class MainLayoutComponent {
+  isSidebarOpen = signal(false);
+
+  toggleSidebar() {
+    this.isSidebarOpen.update((v) => !v);
+  }
+}
